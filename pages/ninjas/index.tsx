fa9180aa1711b2ef31ac03c39cbd402e2daa3e5b
Tanusree Das <tithydas3@gmail.com>
2023-01-24 18:49:37 +0600

    folder create

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** @format */
 
 import Link from "next/link";
@@ -24,3 +25,31 @@ export default function Ninjas({ ninjas }) {
 		</>
 	);
 }
+=======
+/** @format */
+
+import Link from "next/link";
+import { Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+export const getStaticProps = async () => {
+	const res = await fetch("https://63be6aa1585bedcb36acc7d4.mockapi.io/header");
+	const data = await res.json();
+	return {
+		props: { ninjas: data },
+	};
+};
+export default function Ninjas({ ninjas }) {
+	return (
+		<>
+			<h1>About Us</h1>
+			{ninjas.map(ninja => (
+				<Link
+					href={"/ninjas/" + ninja.userid}
+					key={ninja.userid}
+				>
+					<p>{ninja.title}</p>
+				</Link>
+			))}
+		</>
+	);
+}
+>>>>>>> 56b5a69468cb6e38bc1886ee720f3b6d3ba99578
